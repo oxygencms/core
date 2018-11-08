ad17178358e4214f72ca6f4b578193438c6f64d7
Martin Blagoev <mb.reppair@gmail.com>
2018-11-08 15:25:59 +0200

    add activitiy log

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -4,9 +4,18 @@ namespace Oxygencms\Core\Models;
 
 use Oxygencms\Core\Traits\CommonQueries;
 use Oxygencms\Core\Traits\CommonAccessors;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 abstract class Model extends EloquentModel
 {
-    use CommonAccessors, CommonQueries;
+    use CommonAccessors, CommonQueries, LogsActivity;
+
+    /**
+     * Logged attributes.
+     *
+     * @var bool $logUnguarded
+     */
+    protected static $logUnguarded = true;
+    protected static $logFillable = true;
 }

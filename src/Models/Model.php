b0adcbff72f9e83e189c19a80e3e93c7659d1716
Pavel Pavlov <pavlovp@icloud.com>
2020-09-23 15:33:02 +0300

    Fix date serialization

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -2,6 +2,7 @@
 
 namespace Oxygencms\Core\Models;
 
+use DateTimeInterface;
 use Oxygencms\Core\Traits\CommonQueries;
 use Oxygencms\Core\Traits\CommonAccessors;
 use Spatie\Activitylog\Traits\LogsActivity;
@@ -18,4 +19,16 @@ abstract class Model extends EloquentModel
      */
     protected static $logUnguarded = true;
     protected static $logFillable = true;
+
+    /**
+     * Prepare a date for array / JSON serialization.
+     *
+     * @param \DateTimeInterface $date
+     *
+     * @return string
+     */
+    protected function serializeDate(DateTimeInterface $date)
+    {
+        return $date->format('Y-m-d H:i:s');
+    }
 }
